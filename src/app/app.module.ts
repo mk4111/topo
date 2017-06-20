b9a8180758596fab5ace0d038eaf932620757f5a
mk4111 <marie.kasai@live.co.uk>
2017-06-20 11:18:15 +0100

    updates main app with road component provider and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,25 +9,41 @@ import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
 import * as highcharts from 'highcharts';
 
 import { AppComponent } from './app.component';
+import { RoadComponent } from './road/road.component';
+import { RoadService } from './road.service';
+
+const ROUTES = [
+  {
+    path: '',
+    redirectTo: 'roads',
+    pathMatch: 'full'
+  },
+  {
+    path: 'road',
+    component: RoadComponent
+  }
+];
 
 export function highchartsFactory() {
  return highcharts;
 }
 
 @NgModule({
- declarations: [AppComponent],
+ declarations: [AppComponent, RoadComponent],
  imports: [
    BrowserModule,
    ChartModule,
    FormsModule,
    HttpModule,
+   RouterModule.forRoot(ROUTES)
  ],
  providers: [
+   RoadService,
    {
      provide: HighchartsStatic,
      useFactory: highchartsFactory
    }
  ],
- bootstrap: [AppComponent]
+ bootstrap: [AppComponent, RoadComponent]
 })
 export class AppModule { }
