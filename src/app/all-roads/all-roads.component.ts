1030abc850e0ba9538df4034b8b4b4ea0c30fd89
mk4111 <marie.kasai@live.co.uk>
2017-06-21 04:02:28 +0100

    generates all-roads component

---
+++ src/app/all-roads/all-roads.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'all-roads',
+  templateUrl: './all-roads.component.html',
+  styleUrls: ['./all-roads.component.css']
+})
+export class AllRoadsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
